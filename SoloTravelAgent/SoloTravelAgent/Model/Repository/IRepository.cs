f9ffa1c015761ac60b6cb30aa76f249a8001296c
DusanPejanovic <75852858+afilipic@users.noreply.github.com>
2023-05-05 03:47:09 -0700

    added repository

--- SoloTravelAgent/SoloTravelAgent/Model/Repository/IRepository.cs
+++ SoloTravelAgent/SoloTravelAgent/Model/Repository/IRepository.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SoloTravelAgent.Model.Repository
 {
-    internal interface IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        T Get(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
     }
 }
---
+++ SoloTravelAgent/SoloTravelAgent/Model/Repository/Implements/Repository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoloTravelAgent.Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloTravelAgent.Model.Repository.Implements
+{
+
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly TravelSystemDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(TravelSystemDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).ToList();
+        }
+
+        public T Get(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
