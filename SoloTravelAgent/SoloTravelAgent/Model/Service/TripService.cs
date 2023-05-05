8c323cdf40f6c4c5926b48d1f309c4df5fb8b0bc
DusanPejanovic <75852858+afilipic@users.noreply.github.com>
2023-05-05 03:59:42 -0700

    added trip servuce

---
+++ SoloTravelAgent/SoloTravelAgent/Model/Service/TripService.cs
@@ -0,0 +1,48 @@
+﻿using SoloTravelAgent.Model.Data;
+using SoloTravelAgent.Model.Entities;
+using SoloTravelAgent.Model.Repository.Implements;
+using SoloTravelAgent.Model.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloTravelAgent.Model.Service
+{
+    public class TripService
+    {
+        private readonly IRepository<Trip> _tripRepository;
+
+        public TripService(TravelSystemDbContext dbContext)
+        {
+            _tripRepository = new Repository<Trip>(dbContext);
+        }
+
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            return _tripRepository.GetAll();
+        }
+
+        public Trip GetTrip(int id)
+        {
+            return _tripRepository.Get(id);
+        }
+
+        public void AddTrip(Trip trip)
+        {
+            _tripRepository.Add(trip);
+        }
+
+        public void UpdateTrip(Trip trip)
+        {
+            _tripRepository.Update(trip);
+        }
+
+        public void RemoveTrip(Trip trip)
+        {
+            _tripRepository.Remove(trip);
+        }
+    }
+
+}
