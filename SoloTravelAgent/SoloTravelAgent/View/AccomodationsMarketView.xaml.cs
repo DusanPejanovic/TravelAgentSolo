c80a1d15d41fad5a7d49ba4fd1c2563705d85414
DusanPejanovic <95129566+DusanPejanovic@users.noreply.github.com>
2023-06-12 14:24:49 +0200

    added restraunt market view

--- SoloTravelAgent/SoloTravelAgent/View/AccomodationsMarketView.xaml.cs
+++ SoloTravelAgent/SoloTravelAgent/View/AccomodationsMarketView.xaml.cs
@@ -13,34 +13,47 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using SoloTravelAgent.Navigation;
-
+using System.Windows.Shapes;
 
 namespace SoloTravelAgent.View
 {
     /// <summary>
-    /// Interaction logic for RestaurantMarketView.xaml
+    /// Interaction logic for AccomodationsMarketView.xaml
     /// </summary>
-    public partial class RestaurantMarketView : UserControl
+    public partial class AccomodationsMarketView : UserControl
     {
-        private RestaurantViewModel _viewModel;
-        public Trip _selectedTrip;
-        public RestaurantMarketView()
+
+        private AccommodationsViewModel _viewModel;
+        private Trip _selectedTrip;
+
+        public AccomodationsMarketView()
         {
             InitializeComponent();
             Loaded += OnLoaded;
         }
 
+
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            _viewModel = DataContext as RestaurantViewModel;
+            _viewModel = DataContext as AccommodationsViewModel;
             _selectedTrip = _viewModel.SelectedTrip;
         }
 
-        private void AccommodationsRadioButton_Checked(object sender, RoutedEventArgs e)
+        private void BackButtonClicked(Object sender, RoutedEventArgs e)
         {
-            var vm = new AccommodationsViewModel(_selectedTrip);
+            var vm = new TripViewModel();
+            NavigationService.Instance.NavigateTo(vm);
+        }
+
+        public int RestaurantCount
+        {
+            get => _selectedTrip?.Restaurants.Count ?? 0;
+        }
+
+        private void RestaurantRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var vm = new RestaurantViewModel(_selectedTrip);
             NavigationService.Instance.NavigateTo(vm);
         }
 
@@ -50,40 +63,36 @@ namespace SoloTravelAgent.View
             NavigationService.Instance.NavigateTo(vm);
         }
 
-        private void BackButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            var vm = new TripViewModel();
-            NavigationService.Instance.NavigateTo(vm);
-        }
-
         private async void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            var restaurant = (sender as Button).DataContext as Restaurant;
-            if (restaurant == null) return;
+            var accommodation = (sender as Button).DataContext as Accommodation;
+            if (accommodation == null) return;
 
-            var msg = $"Are you sure you want to delete the restaurant: {restaurant.Name}?";
+            var msg = $"Are you sure you want to delete the accommodation: {accommodation.Name}?";
             var result = MessageBox.Show(msg, "Confirm Delete", MessageBoxButton.YesNo);
 
             if (result == MessageBoxResult.Yes)
             {
-                _viewModel.DeleteRestaurantCommand.Execute(restaurant);
+                _viewModel.DeleteAccommodationCommand.Execute(accommodation);
             }
         }
 
         private async void EditButton_Click(object sender, RoutedEventArgs e)
         {
-            var restaurant = (sender as Button).DataContext as Restaurant;
-            if (restaurant == null) return;
-            var dialog = new EditRestaurantDialogView(restaurant, _viewModel);
+            var accommodation = (sender as Button).DataContext as Accommodation;
+            if (accommodation == null) return;
+            var dialog = new EditAccommodationDialogView(accommodation, _viewModel);
             dialog.Owner = Window.GetWindow(this);
             dialog.ShowDialog();
+
         }
 
         private async void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new AddRestaurantDialogView(_viewModel);
+            var dialog = new AddAccommodationDialogView(_viewModel);
             dialog.Owner = Window.GetWindow(this);
             dialog.ShowDialog();
+
         }
 
         private bool IsMaximize = false;
@@ -140,70 +149,48 @@ namespace SoloTravelAgent.View
             if (selectedItem != null && _viewModel != null)
             {
                 string selectedContent = selectedItem.Content.ToString();
-                if (selectedContent == "Italian Cuisine")
+
+                if (selectedContent == "Stars == 5")
                 {
-                    List<Restaurant> filteredRes = FilterItalianCuisine();
-                    UpdateCollection(filteredRes);
+                    List<Accommodation> filteredAcc = FilterBestAccommodation();
+                    UpdateCollection(filteredAcc);
                 }
-                else if (selectedContent == "Mexican Cuisine")
+                else if (selectedContent == "Stars >= 3")
                 {
-                    List<Restaurant> filteredRes = FilterMexicanCuisine();
-                    UpdateCollection(filteredRes);
-                }
-                else if (selectedContent == "Serbian Cuisine")
-                {
-                    List<Restaurant> filteredRes = FilterSerbianCuisine();
-                    UpdateCollection(filteredRes);
-                }
-                else if (selectedContent == "Chinese Cuisine")
-                {
-                    List<Restaurant> filteredRes = FilterChineseCuisine();
-                    UpdateCollection(filteredRes);
+                    List<Accommodation> filteredAcc = FilteBetterThan3();
+                    UpdateCollection(filteredAcc);
                 }
                 else if (selectedContent == "No Filter")
                 {
-                    _viewModel.LoadRestaurants();
+                    _viewModel.LoadAccommodations();
                 }
+
+
             }
         }
 
-        private void UpdateCollection(List<Restaurant> newRestaurants)
+        private List<Accommodation> FilterBestAccommodation()
         {
-            _viewModel.Restaurants.Clear();
-            foreach (var Restaurant in newRestaurants)
+            _viewModel.LoadAccommodations();
+            var Accommodations = _viewModel.Accommodations;
+            return Accommodations.Where(Accommodation => Accommodation.Stars == 5).ToList();
+        }
+
+        private List<Accommodation> FilteBetterThan3()
+        {
+            _viewModel.LoadAccommodations();
+            var Accommodations = _viewModel.Accommodations;
+            return Accommodations.Where(Accommodation => Accommodation.Stars >= 3).ToList();
+        }
+
+
+        private void UpdateCollection(List<Accommodation> newAttractions)
+        {
+            _viewModel.Accommodations.Clear();
+            foreach (var attraction in newAttractions)
             {
-                _viewModel.Restaurants.Add(Restaurant);
+                _viewModel.Accommodations.Add(attraction);
             }
-        }
-
-        private List<Restaurant> FilterItalianCuisine()
-        {
-            _viewModel.LoadRestaurants();
-            var Restaurants = _viewModel.Restaurants;
-            return Restaurants.Where(Restaurant => Restaurant.Cuisine == "Italian").ToList();
-        }
-
-        private List<Restaurant> FilterChineseCuisine()
-        {
-            _viewModel.LoadRestaurants();
-            var Restaurants = _viewModel.Restaurants;
-            return Restaurants.Where(Restaurant => Restaurant.Cuisine == "Chinese").ToList();
-        }
-
-        private List<Restaurant> FilterSerbianCuisine()
-        {
-            _viewModel.LoadRestaurants();
-            var Restaurants = _viewModel.Restaurants;
-            return Restaurants.Where(Restaurant => Restaurant.Cuisine == "Serbian").ToList();
-        }
-
-
-        private List<Restaurant> FilterMexicanCuisine()
-        {
-            _viewModel.LoadRestaurants();
-            var Restaurants = _viewModel.Restaurants;
-            return Restaurants.Where(Restaurant => Restaurant.Cuisine == "Mexican").ToList();
         }
     }
 }
-
---
+++ SoloTravelAgent/SoloTravelAgent/View/TouristAttractionMarketView.xaml.cs
@@ -0,0 +1,185 @@
+﻿using SoloTravelAgent.Model.Entities;
+using SoloTravelAgent.Navigation;
+using SoloTravelAgent.View.DialogView;
+using SoloTravelAgent.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SoloTravelAgent.View
+{
+    /// <summary>
+    /// Interaction logic for TouristAttractionMarketView.xaml
+    /// </summary>
+    public partial class TouristAttractionMarketView : UserControl
+    {
+        private TouristAttractionsViewModel _viewModel;
+        private Trip _selectedTrip;
+
+        public TouristAttractionMarketView()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _viewModel = DataContext as TouristAttractionsViewModel;
+            _selectedTrip = _viewModel.SelectedTrip;
+        }
+
+        private void BackButtonClicked(Object sender, RoutedEventArgs e)
+        {
+            var vm = new TripViewModel();
+            NavigationService.Instance.NavigateTo(vm);
+        }
+
+        private async void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var attraction = (sender as Button).DataContext as TouristAttraction;
+            if (attraction == null) return;
+
+            var msg = $"Are you sure you want to delete the tourist attraction: {attraction.Name}?";
+            var result = MessageBox.Show(msg, "Confirm Delete", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                _viewModel.DeleteTouristAttractionCommand.Execute(attraction);
+            }
+        }
+
+        private bool IsMaximize = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                var window = Window.GetWindow(this);
+                if (IsMaximize)
+                {
+                    window.WindowState = WindowState.Normal;
+                    window.Width = 1080;
+                    window.Height = 720;
+
+                    IsMaximize = false;
+                }
+                else
+                {
+                    window.WindowState = WindowState.Maximized;
+
+                    IsMaximize = true;
+                }
+            }
+        }
+
+        private void AccommodationsRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var vm = new AccommodationsViewModel(_selectedTrip);
+            NavigationService.Instance.NavigateTo(vm);
+        }
+
+        private async void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var attraction = (sender as Button).DataContext as TouristAttraction;
+            if (attraction == null) return;
+            var dialog = new EditAttractionDialogView(attraction, _viewModel);
+            dialog.Owner = Window.GetWindow(this); // ovo mi je roditelj prozor
+            dialog.ShowDialog();
+
+        }
+
+        private async void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new AddAttractionDialogView(_viewModel);
+            dialog.Owner = Window.GetWindow(this);
+            dialog.ShowDialog();
+
+        }
+
+        private void RestaurantRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var vm = new RestaurantViewModel(_selectedTrip);
+            NavigationService.Instance.NavigateTo(vm);
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                Window.GetWindow(this).DragMove();
+            }
+        }
+
+        private void ComboBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            comboBox.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#C88EA7"));
+
+        }
+
+        private void ComboBox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            comboBox.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#917FB3"));
+
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+
+            if (selectedItem != null && _viewModel != null)
+            {
+                string selectedContent = selectedItem.Content.ToString();
+
+                if (selectedContent == "Fee >= 1500")
+                {
+                    List<TouristAttraction> filteredAttractons = FilterPriceGreaterThanOrEqualTo1500();
+                    UpdateTouristCollection(filteredAttractons);
+                }
+                else if (selectedContent == "Fee < 1500")
+                {
+                    List<TouristAttraction> filteredAttr = FilterPriceLowerThan1500();
+                    UpdateTouristCollection(filteredAttr);
+                }
+                else if (selectedContent == "No Filter")
+                {
+                    _viewModel.LoadTouristAttractions();
+                }
+
+            }
+        }
+
+        private List<TouristAttraction> FilterPriceGreaterThanOrEqualTo1500()
+        {
+            _viewModel.LoadTouristAttractions();
+            var attractions = _viewModel.TouristAttractions;
+            return attractions.Where(attraction => attraction.EntryFee >= 1500).ToList();
+        }
+
+        private List<TouristAttraction> FilterPriceLowerThan1500()
+        {
+            _viewModel.LoadTouristAttractions();
+            var attractions = _viewModel.TouristAttractions;
+            return attractions.Where(attraction => attraction.EntryFee < 1500).ToList();
+        }
+
+        private void UpdateTouristCollection(List<TouristAttraction> newAttractions)
+        {
+            _viewModel.TouristAttractions.Clear();
+            foreach (var attraction in newAttractions)
+            {
+                _viewModel.TouristAttractions.Add(attraction);
+            }
+        }
+    }
+}
