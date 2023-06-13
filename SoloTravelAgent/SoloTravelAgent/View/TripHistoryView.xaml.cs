ae51cffb1c91d1bebbc5f51eb164ce4b72343b7c
DusanPejanovic <95129566+DusanPejanovic@users.noreply.github.com>
2023-06-13 06:32:51 +0200

    updated trip history view

--- SoloTravelAgent/SoloTravelAgent/View/TripHistoryView.xaml.cs
+++ SoloTravelAgent/SoloTravelAgent/View/TripHistoryView.xaml.cs
@@ -3,6 +3,7 @@ using SoloTravelAgent.Model.Entities;
 using SoloTravelAgent.Navigation;
 using SoloTravelAgent.View.DialogView;
 using SoloTravelAgent.ViewModel;
+using SoloTravelAgent.ViewModel.BookingManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,164 +25,39 @@ namespace SoloTravelAgent.View
     /// </summary>
     public partial class TripHistoryView : UserControl
     {
-        private readonly TripViewModel _viewModel;
-        private readonly TravelSystemDbContext _dbContext;
-
         public TripHistoryView()
         {
-            InitializeComponent();
-            _viewModel = new TripViewModel();
-            DataContext = _viewModel;
+            
+                InitializeComponent();
+            
         }
 
-        private void ComboBox_MouseEnter(object sender, MouseEventArgs e)
-        {
-            ComboBox comboBox = (ComboBox)sender;
-            comboBox.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#C88EA7"));
-
-        }
-
-        private void ComboBox_MouseLeave(object sender, MouseEventArgs e)
-        {
-            ComboBox comboBox = (ComboBox)sender;
-            comboBox.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#917FB3"));
-
-        }
-
-        private async void AddButton_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new SoloTravelAgent.View.DialogView.StepperView.AddTripDialogView(_viewModel, _dbContext);
-            dialog.Owner = Window.GetWindow(this);
-            dialog.ShowDialog();
-        }
-
-        private async void EditButton_Click(object sender, RoutedEventArgs e)
-        {
-            var trip = (sender as Button).DataContext as Trip;
-            if (trip == null) return;
-            var dialog = new EditTripView(trip, _viewModel);
-            dialog.Owner = Window.GetWindow(this); // ovo mi je roditelj prozor
-            dialog.ShowDialog();
-        }
-
-        private async void DeleteButton_Click(object sender, RoutedEventArgs e)
-        {
-            var trip = (sender as Button).DataContext as Trip;
-            if (trip == null) return;
-
-            var msg = $"Are you sure you want to delete the tourist attraction: {trip.Name}?";
-            var result = MessageBox.Show(msg, "Confirm Delete", MessageBoxButton.YesNo);
-
-            if (result == MessageBoxResult.Yes)
+            private void ApproveBooking(object sender, RoutedEventArgs e)
             {
-                _viewModel.DeleteTripCommand.Execute(trip);
+                var booking = (sender as Button).DataContext as Booking;
+                if (booking == null) return;
+
+
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to approve this booking?", "Confirmation", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    var viewModel = DataContext as AgentBookingManagementViewModel;
+                    viewModel.ApproveBooking(booking.Id);
+                }
             }
-        }
 
-        private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            var selectedTrip = (sender as DataGrid)?.SelectedItem as Trip;
-
-            if (selectedTrip != null)
+            private void RejectBooking(object sender, RoutedEventArgs e)
             {
-                var restaurantViewModel = new RestaurantViewModel(selectedTrip);
-                NavigationService.Instance.NavigateTo(restaurantViewModel);
-            }
-        }
+                var booking = (sender as Button).DataContext as Booking;
+                if (booking == null) return;
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox comboBox = (ComboBox)sender;
-            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
 
-            if (selectedItem != null && _viewModel != null)
-            {
-                string selectedContent = selectedItem.Content.ToString();
-
-                if (selectedContent == "Price >= 1500")
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to reject this booking?", "Confirmation", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
                 {
-                    List<Trip> filteredTrips = FilterPriceGreaterThanOrEqualTo1500();
-                    UpdateTripsCollection(filteredTrips);
-                }
-                else if (selectedContent == "Price < 1500")
-                {
-                    List<Trip> filteredTrips = FilterPriceLowerThan1500();
-                    UpdateTripsCollection(filteredTrips);
-                }
-                else if (selectedContent == "Next Week")
-                {
-                    List<Trip> filteredTrips = FilterTripsNextWeek();
-                    UpdateTripsCollection(filteredTrips);
-                }
-                else if (selectedContent == "This Month")
-                {
-                    List<Trip> filteredTrips = FilterTripsThisMonth();
-                    UpdateTripsCollection(filteredTrips);
-                }
-                else if (selectedContent == "Next Month")
-                {
-                    List<Trip> filteredTrips = FilterTripsNextMonth();
-                    UpdateTripsCollection(filteredTrips);
-                }
-                else if (selectedContent == "No Filter")
-                {
-                    _viewModel.LoadTrips();
+                    var viewModel = DataContext as TripBookedViewModel;
+                    viewModel.DeleteBooking(booking.Id);
                 }
             }
         }
-
-        private List<Trip> FilterTripsThisMonth()
-        {
-            _viewModel.LoadTrips();
-            var trips = _viewModel.Trips;
-            var today = DateTime.Today;
-            var firstDayOfMonth = new DateTime(today.Year, today.Month, 1);
-            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
-            return trips.Where(trip => trip.StartDate >= firstDayOfMonth && trip.StartDate <= lastDayOfMonth).ToList();
-        }
-
-        private List<Trip> FilterTripsNextMonth()
-        {
-            _viewModel.LoadTrips();
-            var trips = _viewModel.Trips;
-            var today = DateTime.Today;
-            var firstDayOfNextMonth = new DateTime(today.Year, today.Month, 1).AddMonths(1);
-            var lastDayOfNextMonth = firstDayOfNextMonth.AddMonths(1).AddDays(-1);
-            return trips.Where(trip => trip.StartDate >= firstDayOfNextMonth && trip.StartDate <= lastDayOfNextMonth).ToList();
-        }
-
-
-        private List<Trip> FilterTripsNextWeek()
-        {
-            _viewModel.LoadTrips();
-            var trips = _viewModel.Trips;
-            DateTime today = DateTime.Today;
-            DateTime oneWeekFromNow = today.AddDays(7);
-            return trips.Where(trip => trip.StartDate >= today && trip.StartDate <= oneWeekFromNow).ToList();
-        }
-
-
-        private void UpdateTripsCollection(List<Trip> newTrips)
-        {
-            _viewModel.Trips.Clear();
-            foreach (var trip in newTrips)
-            {
-                _viewModel.Trips.Add(trip);
-            }
-        }
-
-        private List<Trip> FilterPriceGreaterThanOrEqualTo1500()
-        {
-            _viewModel.LoadTrips();
-            var trips = _viewModel.Trips;
-            return trips.Where(trip => trip.Price >= 1500).ToList();
-        }
-
-        private List<Trip> FilterPriceLowerThan1500()
-        {
-            _viewModel.LoadTrips();
-            var trips = _viewModel.Trips;
-            return trips.Where(trip => trip.Price < 1500).ToList();
-        }
-    }
 }
