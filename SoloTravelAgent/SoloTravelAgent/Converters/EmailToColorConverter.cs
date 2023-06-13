5c243c8d88bb0929b230968134568b3c3b9a147d
serbedzijajovan <100165422+serbedzijajovan@users.noreply.github.com>
2023-06-13 07:11:03 +0200

    Added more Help infos

--- SoloTravelAgent/SoloTravelAgent/Converters/EmailToColorConverter.cs
+++ SoloTravelAgent/SoloTravelAgent/Converters/EmailToColorConverter.cs
@@ -9,17 +9,34 @@ namespace SoloTravelAgent.Converters
     public class EmailToColorConverter : IValueConverter
     {
         private static readonly Dictionary<char, Brush> CharacterColors = new Dictionary<char, Brush>()
-    {
-        { 'J', (Brush)new BrushConverter().ConvertFromString("#1098AD") },
-        { 'R', (Brush)new BrushConverter().ConvertFromString("#1E88E5") },
-        { 'D', (Brush)new BrushConverter().ConvertFromString("#FF8F00") },
-        { 'G', (Brush)new BrushConverter().ConvertFromString("#FF5252") },
-        { 'L', (Brush)new BrushConverter().ConvertFromString("#0CA678") },
-        { 'B', (Brush)new BrushConverter().ConvertFromString("#6741D9") },
-        { 'S', (Brush)new BrushConverter().ConvertFromString("#FF6D00") },
-        { 'A', (Brush)new BrushConverter().ConvertFromString("#FF5252") },
-        { 'F', (Brush)new BrushConverter().ConvertFromString("#1E88E5") },
-    };
+        {
+            { 'J', (Brush)new BrushConverter().ConvertFromString("#1098AD") },
+            { 'M', (Brush)new BrushConverter().ConvertFromString("#1098AD") },
+            { 'O', (Brush)new BrushConverter().ConvertFromString("#1098AD") },
+            { 'R', (Brush)new BrushConverter().ConvertFromString("#1E88E5") },
+            { 'C', (Brush)new BrushConverter().ConvertFromString("#1E88E5") },
+            { 'D', (Brush)new BrushConverter().ConvertFromString("#FF8F00") },
+            { 'K', (Brush)new BrushConverter().ConvertFromString("#FF8F00") },
+            { 'G', (Brush)new BrushConverter().ConvertFromString("#FF5252") },
+            { 'N', (Brush)new BrushConverter().ConvertFromString("#FF5252") },
+            { 'L', (Brush)new BrushConverter().ConvertFromString("#0CA678") },
+            { 'B', (Brush)new BrushConverter().ConvertFromString("#6741D9") },
+            { 'I', (Brush)new BrushConverter().ConvertFromString("#6741D9") },
+            { 'S', (Brush)new BrushConverter().ConvertFromString("#FF6D00") },
+            { 'A', (Brush)new BrushConverter().ConvertFromString("#FF5252") },
+            { 'P', (Brush)new BrushConverter().ConvertFromString("#FF5252") },
+            { 'F', (Brush)new BrushConverter().ConvertFromString("#1E88E5") },
+            { 'H', (Brush)new BrushConverter().ConvertFromString("#1E88E5") },
+            { 'E', (Brush)new BrushConverter().ConvertFromString("#1E88E5") },
+            { 'Q', (Brush)new BrushConverter().ConvertFromString("#1098AD") },
+            { 'T', (Brush)new BrushConverter().ConvertFromString("#FF8F00") }, 
+            { 'U', (Brush)new BrushConverter().ConvertFromString("#FF5252") }, 
+            { 'V', (Brush)new BrushConverter().ConvertFromString("#0CA678") }, 
+            { 'W', (Brush)new BrushConverter().ConvertFromString("#6741D9") }, 
+            { 'X', (Brush)new BrushConverter().ConvertFromString("#FF6D00") },
+            { 'Y', (Brush)new BrushConverter().ConvertFromString("#FF5252") }, 
+            { 'Z', (Brush)new BrushConverter().ConvertFromString("#1E88E5") }, 
+        };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
