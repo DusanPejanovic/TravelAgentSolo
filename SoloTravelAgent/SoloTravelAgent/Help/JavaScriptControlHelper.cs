9af4e357d7a831dfcd673f91050c33763d21dc9f
serbedzijajovan <100165422+serbedzijajovan@users.noreply.github.com>
2023-06-13 06:33:05 +0200

    First part of help implemented

---
+++ SoloTravelAgent/SoloTravelAgent/Help/JavaScriptControlHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SoloTravelAgent.Help
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [ComVisible(true)]
+    public class JavaScriptControlHelper
+    {
+        MainWindow prozor;
+        public JavaScriptControlHelper(MainWindow w)
+        {
+            prozor = w;
+        }
+    }
+}
